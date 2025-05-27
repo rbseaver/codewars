c838b232c261bc1abd5a0baa2bde9f213fcf9616
Rob Seaver <rob.seaver@gmail.com>
2025-05-27 17:01:44 -0500

    test: streamline odd-even tests

--- codewars.oddoreven/kata.spec.ts
+++ codewars.oddoreven/kata.spec.ts
@@ -1,43 +1,15 @@
 import { oddOrEven } from './kata';
 
 describe('when determining if sum of array elements is odd or even', () => {
-  it('should return "even" if the sum is even', () => {
-    const input = [1, 1];
-
+  it.each([
+    [[1, 1], 'even'],
+    [[1, 2], 'odd'],
+    [[1023, 1, 3], 'odd'],
+    [[0, -1, -3], 'even'],
+    [[], 'even']
+  ])('"%d" should be "%s"', (input: number[], expected: string) => {
     const result = oddOrEven(input);
 
-    expect(result).toBe('even');
+    expect(result).toBe(expected);
   });
-
-  it('should return "odd" if the sum is odd', () => {
-    const input = [1, 2];
-
-    const result = oddOrEven(input);
-
-    expect(result).toBe('odd');
-  });
-
-  it('more "odd" tests', () => {
-    const input = [1023, 1, 3];
-
-    const result = oddOrEven(input);
-
-    expect(result).toBe('odd');
-  });
-
-  it('negative numbers', () => {
-    const input = [0, -1, -3];
-
-    const result = oddOrEven(input);
-
-    expect(result).toBe('even');
-  })
-
-  it('empty array should be considered [0]', () => {
-    const input: number[] = [];
-
-    const result = oddOrEven(input);
-
-    expect(result).toBe('even');
-  })
 })
