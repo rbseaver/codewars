59bae2d1d1b1adf562a7ad53024e4fd24424a2d2
Rob Seaver <rob.seaver@gmail.com>
2025-05-27 12:47:32 -0500

    chore: fresh start; new me

--- kata.population-growth/kata.spec.ts
+++
@@ -1,9 +0,0 @@
-import { nbYear } from './kata';
-
-describe('predicting time to grow to a certain population', () => {
-  test('should return the number of years needed to reach specified population', () => {
-    const result = nbYear(1500, 5, 100, 5000);
-
-    expect(result).toBe(15);
-  });
-});
--- kata.population-growth/kata.ts
+++
@@ -1,4 +0,0 @@
-export const nbYear = (p0: number, percent: number, aug: number, p: number): number => {
-  // your code
-  return 15;
-}
