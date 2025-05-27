d69b14a4717f5e42b358b3273c75a446989b5379
Rob Seaver <rob.seaver@gmail.com>
2025-05-27 15:31:20 -0500

    feat: pascal case converter

--- codewars.pascalcase/kata.spec.ts
+++ codewars.pascalcase/kata.spec.ts
@@ -1,11 +1,27 @@
 import { toPascalCase } from './kata';
 
 describe('when converting to camel case', () => {
-  it('should do the thing!', () => {
-    const input = 'derp is my favorite word';
+  it.each([
+    ['derp is my favorite word', 'DerpIsMyFavoriteWord'],
+    ['I like camels', 'ILikeCamels']
+  ])('should convert "%s" to "%s"', (input, expected) => {
+    const result = toPascalCase(input);
+    expect(result).toBe(expected);
+  });
+
+  it('should return empty string if input is empty', () => {
+    const input = '';
 
     const result = toPascalCase(input);
 
-    expect(result).toBe('DerpIsMyFavoriteWord');
+    expect(result).toBe('');
+  });
+
+  it('should gracefully handle empty words or multiple spaces', () => {
+    const input = 'this has  more than one space  ';
+
+    const result = toPascalCase(input);
+
+    expect(result).toBe('ThisHasMoreThanOneSpace');
   })
 });
--- codewars.pascalcase/kata.ts
+++ codewars.pascalcase/kata.ts
@@ -1,3 +1,11 @@
 export const toPascalCase = (value: string): string => {
-  return 'DerpIsMyFavoriteWord';
+  if (value === '') {
+    return '';
+  }
+
+  const words = value.split(/\s+/).filter(Boolean);
+
+  return words
+    .map(word => `${word[0]?.toUpperCase()}${word.substring(1)}`)
+    .join('');
 }
