b97f8cb223e0d66815fe83759e53fb2685b3a91c
Rob Seaver <rob.seaver@gmail.com>
2025-05-27 14:29:41 -0500

    feat: narcissistic number kata

--- codewars.narcissisticnumber/kata.spec.ts
+++ codewars.narcissisticnumber/kata.spec.ts
@@ -4,6 +4,18 @@ describe('when finding the narcissistic number', () => {
   it('should return true for 153', () => {
     const result = narcissistic(153);
     
-    expect(result).toBe(true);
+    expect(result).toBeTruthy();
   });
+
+  it('should return true for 1634', () => {
+    const result = narcissistic(1634);
+
+    expect(result).toBeTruthy();
+  });
+
+  it('should return false for 1652 ', () => {
+    const result = narcissistic(1652);
+
+    expect(result).toBeFalsy();
+  })
 });
--- codewars.narcissisticnumber/kata.ts
+++ codewars.narcissisticnumber/kata.ts
@@ -1,3 +1,13 @@
 export const narcissistic = (value: number): boolean => {
-  return false;
+  const digits = value.toString()
+    .split('')
+    .map(Number);
+
+  const power = digits.length;
+
+  let sum = digits.reduce((acc, digit) => {
+    return acc + Math.pow(digit, power)
+  }, 0);
+
+  return sum === value;
 }
