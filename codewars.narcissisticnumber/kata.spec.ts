0ad9be8cdd39c94045e71287ddcd32602565db8a
Rob Seaver <rob.seaver@gmail.com>
2025-05-27 15:29:45 -0500

    test: use it.each for narcissistic number test

--- codewars.narcissisticnumber/kata.spec.ts
+++ codewars.narcissisticnumber/kata.spec.ts
@@ -1,21 +1,13 @@
 import { narcissistic } from './kata';
 
 describe('when finding the narcissistic number', () => {
-  it('should return true for 153', () => {
-    const result = narcissistic(153);
-    
-    expect(result).toBeTruthy();
+  it.each([
+    [153, true],
+    [1634, true],
+    [1652, false]
+  ])('"%d" should be "%b"', (input, expected) => {
+    const result = narcissistic(input);
+
+    expect(result).toBe(expected);
   });
-
-  it('should return true for 1634', () => {
-    const result = narcissistic(1634);
-
-    expect(result).toBeTruthy();
-  });
-
-  it('should return false for 1652 ', () => {
-    const result = narcissistic(1652);
-
-    expect(result).toBeFalsy();
-  })
 });
