c97f75c4e8587f3856b87a39c02754f3ba9c2406
Rob Seaver <rob.seaver@gmail.com>
2025-05-27 15:48:45 -0500

    docs: add comments to explain issue submitting solution to Codewars

--- codewars.pascalcase/kata.spec.ts
+++ codewars.pascalcase/kata.spec.ts
@@ -1,5 +1,17 @@
 import { toPascalCase } from './kata';
 
+/* This is for the following kata: https://www.codewars.com/kata/587731fda577b3d1b0001196
+This achieves what the instructions ask for, and all the cases shown in the pre-submimssion tests.
+
+However, when submitting (not pre-submission tests), there is a random test failure for "random string",
+but it is not known what inputs the test is using. The test fails, but all that it indicates
+is that the output does not match the expected output. So, this solution fails on submit, but meets
+all the outlined specs.
+
+I'm committing it to the solutions anyway. It's quite possible I'm missing something obvious, and
+I will return and correct this if if I discover the input that's being tested.
+*/
+
 describe('when converting to camel case', () => {
   it.each([
     ['derp is my favorite word', 'DerpIsMyFavoriteWord'],
