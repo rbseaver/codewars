3307585f8defa0602784fd8f6f4cc5be812605c1
Rob Seaver <rob.seaver@gmail.com>
2025-05-28 16:03:24 -0500

    feat: sum the two smallest numbers of an array

--- codewards.sumlowestpositives/kata.spec.ts
+++ codewards.sumlowestpositives/kata.spec.ts
@@ -4,7 +4,12 @@ describe('when finding the sum of the two smallest numbers in an array', () => {
   it.each([
     [[19, 5, 42, 2, 77], 7],
     [[988, 45, 700, 123], 168],
-    [[10, 343445353, 3453445, 3453545353453], 3453455]
+    [[10, 343445353, 3453445, 3453545353453], 3453455],
+    [[5, 8, 12, 19, 22], 13],
+    [[15, 28, 4, 2, 43], 6],
+    [[3, 87, 45, 12, 7], 10],
+    [[23, 71, 33, 82, 1], 24],
+    [[52, 76, 14, 12, 4], 16]
   ])('[%s] should return %s', (input, expected) => {
     const result = sumTwoSmallestNumbers(input);
 
--- codewards.sumlowestpositives/kata.ts
+++ codewards.sumlowestpositives/kata.ts
@@ -1,5 +1,10 @@
 export const sumTwoSmallestNumbers = (numbers: number[]): number => {
-  const sorted = numbers.sort((a, b) => a - b);
-
-  return sorted[0] + sorted[1];
+  const [x, y] = numbers
+    .sort((a, b) => a - b) // Sort small to large
+    .slice(0, 2); // No mutation!
+  
+  // If we returned an array of e.g., `twoSmallest` above,
+  // we could do something fancy here: //twoSmallest.reduce((sum, current) => sum += current, 0);
+  // But, .reduce() is overkill for summing exactly two numbers
+  return x + y;  
 }
