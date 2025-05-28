74f276f161e3df7312fc91dbabe22bc10221930e
Rob Seaver <rob.seaver@gmail.com>
2025-05-28 16:07:32 -0500

    fix: remove .slice() as array destructuring gets us the first two numbers

--- codewards.sumlowestpositives/kata.ts
+++ codewards.sumlowestpositives/kata.ts
@@ -1,10 +1,8 @@
 export const sumTwoSmallestNumbers = (numbers: number[]): number => {
-  const [x, y] = numbers
-    .sort((a, b) => a - b) // Sort small to large
-    .slice(0, 2); // No mutation!
-  
+  const [x, y] = numbers.sort((a, b) => a - b)
+
   // If we returned an array of e.g., `twoSmallest` above,
   // we could do something fancy here: //twoSmallest.reduce((sum, current) => sum += current, 0);
   // But, .reduce() is overkill for summing exactly two numbers
-  return x + y;  
+  return x + y;
 }
