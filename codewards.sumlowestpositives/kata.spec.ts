42545ef0d0df6c81e962eab29c55918bb634e25c
Rob Seaver <rob.seaver@gmail.com>
2025-05-28 15:12:15 -0500

    test: one more test case for good measure

--- codewards.sumlowestpositives/kata.spec.ts
+++ codewards.sumlowestpositives/kata.spec.ts
@@ -3,7 +3,8 @@ import { sumTwoSmallestNumbers } from './kata'
 describe('when finding the sum of the two smallest numbers in an array', () => {
   it.each([
     [[19, 5, 42, 2, 77], 7],
-    [[988, 45, 700, 123], 168]
+    [[988, 45, 700, 123], 168],
+    [[10, 343445353, 3453445, 3453545353453], 3453455]
   ])('[%s] should return %s', (input, expected) => {
     const result = sumTwoSmallestNumbers(input);
 
