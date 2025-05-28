ea2abf19b1e1b2fc8124a4ed9ae5d0712fb34449
Rob Seaver <rob.seaver@gmail.com>
2025-05-28 15:08:31 -0500

    test: refactor test

--- codewards.sumlowestpositives/kata.spec.ts
+++ codewards.sumlowestpositives/kata.spec.ts
@@ -1,19 +1,12 @@
 import { sumTwoSmallestNumbers } from './kata'
 
-describe('finding the lowest positive integers', () => {
-  it('should return 7 for [19, 5, 42, 2, 77]', () => {
-    const input = [19, 5, 42, 2, 77]
-
+describe('when finding the sum of the two smallest numbers in an array', () => {
+  it.each([
+    [[19, 5, 42, 2, 77], 7],
+    [[988, 45, 700, 123], 168]
+  ])('[%s] should return %s', (input, expected) => {
     const result = sumTwoSmallestNumbers(input);
 
-    expect(result).toBe(7);
+    expect(result).toBe(expected);
   });
-
-  it('should return 168 for [988, 45, 700, 123]', () => {
-    const input = [988, 45, 700, 123];
-
-    const result = sumTwoSmallestNumbers(input);
-
-    expect(result).toBe(168);
-  });
-})
+});
