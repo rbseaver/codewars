0afef276517da1fdded4ad9bbf4087b59edc3196
Rob Seaver <rob.seaver@gmail.com>
2025-05-28 15:06:37 -0500

    test: add a second test to verify additional case

--- codewards.sumlowestpositives/kata.spec.ts
+++ codewards.sumlowestpositives/kata.spec.ts
@@ -7,5 +7,13 @@ describe('finding the lowest positive integers', () => {
     const result = sumTwoSmallestNumbers(input);
 
     expect(result).toBe(7);
-  })
+  });
+
+  it('should return 168 for [988, 45, 700, 123]', () => {
+    const input = [988, 45, 700, 123];
+
+    const result = sumTwoSmallestNumbers(input);
+
+    expect(result).toBe(168);
+  });
 })
